da048192ca9f4ba4d3d10633e1e15b1f03cff1f3
ShiftyKitty <cathalpmcgahan@gmail.com>
2021-11-18 21:43:18 +0000

    basic notification system put in place

--- cart/views.py
+++ cart/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import (
     render, redirect, reverse, HttpResponse, get_object_or_404
 )
 from django.contrib import messages
+
 from products.models import Product
 
-# Create your views here.
 
 def view_cart(request):
     """ A view that renders the cart contents page """
@@ -13,8 +13,9 @@ def view_cart(request):
 
 
 def add_to_cart(request, item_id):
-    """ Add a quantity of the specified product to the cart """
+    """ Add a quantity of the specified product to the shopping cart """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -26,23 +27,38 @@ def add_to_cart(request, item_id):
         if item_id in list(cart.keys()):
             if size in cart[item_id]['items_by_size'].keys():
                 cart[item_id]['items_by_size'][size] += quantity
+                messages.success(request,
+                                 (f'Updated size {size.upper()} '
+                                  f'{product.name} quantity to '
+                                  f'{cart[item_id]["items_by_size"][size]}'))
             else:
                 cart[item_id]['items_by_size'][size] = quantity
+                messages.success(request,
+                                 (f'Updated size {size.upper()} '
+                                  f'{product.name} in your cart'))
         else:
             cart[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request,
+                             (f'Added size {size.upper()} '
+                              f'{product.name} to your cart'))
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
+            messages.success(request,
+                             (f'Updated {product.name} '
+                              f'quantity to {cart[item_id]}'))
         else:
             cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
 
 
 def adjust_cart(request, item_id):
-    """ Adjust quantity of the specified product in the cart """
+    """Adjust the quantity of the specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
     if 'product_size' in request.POST:
