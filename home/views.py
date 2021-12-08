3975323fd8ddd8875d3611d022a777d1ca498911
ShiftyKitty <cathalpmcgahan@gmail.com>
2021-12-08 00:11:18 +0000

    changes made to homepage and homepage views.py

--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from elements.models import Element
+from elements.views import element
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    elements = Element.objects.all()
+
+    context = {
+        'elements': elements,
+    }
+
+    return render(request, 'home/index.html', context)
