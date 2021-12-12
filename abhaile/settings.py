cdde448847e07ba1b9620a9aacfe23ef0338aec9
ShiftyKitty <cathalpmcgahan@gmail.com>
2021-12-12 00:56:30 +0000

    removed secret key and set debug

--- abhaile/settings.py
+++ abhaile/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(m8h=cqlbn7u^5hsohafj@j3==1!v4mg6^g)@wi!8r54b()nia'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-abhaile.herokuapp.com', 'localhost']
 
